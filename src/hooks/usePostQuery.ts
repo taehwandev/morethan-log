7de8d544c99fc4a3b1499f68f7391b915637c3c9
TaeHwan <taehwan@thdev.net>
2024-03-10 13:09:06 +0900

    Update usePostQuery.ts

--- src/hooks/usePostQuery.ts
+++ src/hooks/usePostQuery.ts
@@ -7,20 +7,17 @@ const usePostQuery = () => {
   const router = useRouter()
   const { slug } = router.query
 
- // slug가 정의되지 않았을 때 예외처리
-  if (!slug) {
-    return null;
-  }
-
-  const slugs = slug.split('/');
-  if (slugs.length > 1) {
-    const newSlug = slugs[slugs.length - 1];
-    router.push(newSlug);
-  }
-
   const { data } = useQuery<PostDetail>({
     queryKey: queryKey.post(`${slug}`),
     enabled: false,
+    onError: () => {
+      // Redirection logic
+      const slugs = slug.split('/');
+      if (slugs.length > 1) {
+        const newSlug = slugs[slugs.length - 1];
+        router.push(newSlug); 
+      }
+    },
   })
 
   return data
